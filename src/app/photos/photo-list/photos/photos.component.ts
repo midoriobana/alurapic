b24ad8779406f2ecb689aa82be1faed9d6810abc
midoriobana <ingridmidori@live.com>
2022-11-16 21:43:08 -0300

    organizando arquivos

---
+++ src/app/photos/photo-list/photos/photos.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-photos',
+  templateUrl: './photos.component.html'
+})
+export class PhotosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
