c115e99f8f697239e2bfaed943b0c1178b386f39
midoriobana <ingridmidori@live.com>
2022-11-16 21:10:41 -0300

    Criação de rotas

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,11 +1,13 @@
+import { PhotoListComponent } from './../photo/photo-list/photo-list.component';
 import { NgModule } from "@angular/core";
 
 import { PhotoComponent } from "./photo/photo.component";
 import { HttpClientModule } from "@angular/common/http";
+import { PhotoFormComponent } from './photo-form/photo-form.component';
+import { CommonModule } from "@angular/common";
 
 @NgModule({
-    declarations: [ PhotoComponent ],
-    exports: [ PhotoComponent ],
-    imports: [ HttpClientModule ]
+    declarations: [ PhotoComponent, PhotoListComponent, PhotoFormComponent ],
+    imports: [ HttpClientModule, CommonModule]
 })
 export class PhotosModule {}
